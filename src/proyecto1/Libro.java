d78d04bbc3fa3c31acffb20be930b7dd3640f7cb
WiizMark <marcosdos.md@gmail.com>
2026-09-17 20:07:52 +0200

    cuarto commit

---
+++ src/proyecto1/Libro.java
@@ -0,0 +1,23 @@
+package proyecto1;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Libro {
+    protected String id;
+    protected String nombre;
+    protected String autor;
+    protected double precio;
+    protected int stock;
+
+    public Libro(String id, String nombre, String autor, double precio, int stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.autor = autor;
+        this.precio = precio;
+        this.stock = stock;
+    }
+    
+    
+}
--- src/proyecto1/LibroRepository.java
+++ src/proyecto1/LibroRepository.java
@@ -5,10 +5,5 @@ package proyecto1;
  * @author Marcos
  */
 public interface LibroRepository {
-    protected String id;
-    protected String titulo;
-    protected String autor;
-    protected 
-    protected 
     
 }
