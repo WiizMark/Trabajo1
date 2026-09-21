48e9d5e8e81f6be4f36edcac2b9eb92ecef90ee1
WiizMark <marcosdos.md@gmail.com>
2026-09-21 16:02:46 +0200

    ConexionBD añadida

---
+++ src/util/ConexionBD.java
@@ -0,0 +1,24 @@
+package util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConexionBD {
+
+
+	private static final String URL = "jdbc:mysql://localhost:3306/interfaces";
+	private static final String USER = "root";
+	private static final String PASS = "1234";
+
+
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(URL, USER, PASS);
+		} catch (SQLException e) {
+	System.out.println("Error al conectar con la base de datos: " + e.getMessage());
+		}
+		return con;
+	}
+}
