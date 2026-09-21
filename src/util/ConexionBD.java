cdde6eac6132090984cb9a3787745638075a8662
WiizMark <marcosdos.md@gmail.com>
2026-09-21 16:04:59 +0200

    ConexionBD cambiada

--- src/util/ConexionBD.java
+++ src/util/ConexionBD.java
@@ -9,7 +9,7 @@ public class ConexionBD {
 
 	private static final String URL = "jdbc:mysql://localhost:3306/interfaces";
 	private static final String USER = "root";
-	private static final String PASS = "1234";
+	private static final String PASS = "izquierdo";
 
 
 	public static Connection getConnection() {
