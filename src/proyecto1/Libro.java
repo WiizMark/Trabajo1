69394f1edfbabcb08c0d6d76e8f604f9795d03bc
WiizMark <marcosdos.md@gmail.com>
2026-09-21 16:39:37 +0200

    libro completo

--- src/proyecto1/Libro.java
+++ src/proyecto1/Libro.java
@@ -28,4 +28,51 @@ public class Libro {
     
         public Libro() {
     }   
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" + "id=" + id + ", nombre=" + nombre + ", autor=" + autor + ", precio=" + precio + ", stock=" + stock + '}';
+    }
+        
+    
 }
