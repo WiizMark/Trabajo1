d628634c5c2f27ac3c1c084f54b97c19a5856c79
roterfile81 <prologuefile@gmail.com>
2026-09-24 18:43:39 +0200

    funcionacasix

--- src/proyecto1/LibroRepositoryArchivo.java
+++ src/proyecto1/LibroRepositoryArchivo.java
@@ -1,6 +1,42 @@
 package proyecto1;
+import proyecto1.Libro;
+import proyecto1.LibroRepository;
 
-public class LibroRepositoryArchivo {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+public class LibroRepositoryArchivo implements LibroRepository<Object> {
+
+    @Override
+    public boolean insertar(Object objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Object> obtenerTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object obtenerPorId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean actualizar(Object objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    private Libro mapear(ResultSet rs) throws SQLException {
+		return new Libro(
+				
+		);
+	}
 
 
 }
