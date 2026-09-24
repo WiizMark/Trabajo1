2fdf05c4e9c6cf989499e966331e45d6d7ab518a
roterfile81 <prologuefile@gmail.com>
2026-09-24 18:34:03 +0200

    DAO

--- src/proyecto1/LibroRepository.java
+++ src/proyecto1/LibroRepository.java
@@ -1,7 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
 package proyecto1;
 
-import java.util.List;
-
+/**
+ *
+ * @author 2DAM
+ */
 public interface LibroRepository {
-   
+    
 }
